d54aabba7434010c56fb34932a43273bfc6de397
Henrixounez <henrix.ounez@yahoo.fr>
2019-09-23 19:14:03 +0200

    [ADD]: Typescript React and Queries to fetch data

--- src/index.tsx
+++ src/index.tsx
@@ -8,20 +8,23 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
-})
+});
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-})
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
-)
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
