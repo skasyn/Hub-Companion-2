76d10f2e47f3ba1f0459c5c72c8381dd04bd5ed9
Henrixounez <henrix.ounez@yahoo.fr>
2019-11-25 11:32:45 +0100

    [ADD] Mutation to create maker project

--- server/src/resolvers/Mutation.ts
+++ server/src/resolvers/Mutation.ts
@@ -14,14 +14,31 @@ export async function refresh(parent, args, context, userId) {
   return true;
 }
 
-export async function submitMaker(parent, args, context, userId) {
+async function submitMaker(parent, args, context, userId) {
   if (userId === undefined || userId === '' || args.data === undefined || args.data === '') {
     throw new Error('Empty code');
   }
   const data = JSON.parse(args.data);
+  try {
+    await prisma.createMaker(
+      {
+        title: data.title,
+        description: data.description,
+        co_workers: { set: data.co_workers },
+        functionalities: data.functionalities,
+        technologies: data.technologies,
+        resources: data.resources,
+        informations: data.informations,
+        status: 0
+      }
+    )
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
 
-export async function submitSharing(parent, args, context, userId) {
+async function submitSharing(parent, args, context, userId) {
   if (userId === undefined || userId === '' || args.data === undefined || args.data === '') {
     throw new Error('Empty code');
   }
